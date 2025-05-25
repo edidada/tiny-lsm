70dfb642dbf7d12fdf5962417b1c32fbb94a3db7
Vanilla-Beauty <Vanilla-Beauty@users.noreply.github.com>
2025-05-25 10:34:23 +0000

    deploy: d28d9e22bf1c4440f5afa0cd5b00e2fa42863179

--- book/src/giscus.js
+++ book/src/giscus.js
@@ -2,7 +2,7 @@
 window.addEventListener('DOMContentLoaded', function () {
   const giscus = document.createElement('script');
   giscus.src = 'https://giscus.app/client.js';
-  giscus.setAttribute('data-repo', 'ToniXWD/toni-lsm');
+  giscus.setAttribute('data-repo', 'Vanilla-Beauty/tiny-lsm');
   giscus.setAttribute('data-repo-id', 'R_kgDONvdi9Q');
   giscus.setAttribute('data-category', 'Announcements');
   giscus.setAttribute('data-category-id', 'DIC_kwDONvdi9c4CqDu8');
